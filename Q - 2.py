9544834addcd2f3b7c0d61a7354666e2715bf572
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:37:02 +0530

    Update Q - 2.py

--- Q - 2.py
+++ Q - 2.py
@@ -17,4 +17,10 @@ elif y<z and y<x :
     print("Smallest number is",y)
 else :
     print("Smallest number is",z)        
-    
+
+#OUTPUT:
+# What is X ? :32
+# What is Y ? :53
+# What is Z ? :2
+# Largest number is 53
+# Smallest number is 2 
