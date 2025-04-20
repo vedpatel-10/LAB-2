e79b01eff0ebb5bdb9622cd8eb487bbfac61ce5f
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:39:25 +0530

    Update Q - 7.py

--- Q - 7.py
+++ Q - 7.py
@@ -4,4 +4,7 @@ if (year % 4 ==0 and year % 100 !=0) or (year%400 == 0):
     print("Leap Year!")
 else:
     print("Not a Leap Year!")
-        
+
+#OUTPUT:
+# Enter an year: 2026
+# Not a Leap Year!  
