26597859e0de1db23bf6648b59eb73f8c8cb2c24
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:39:45 +0530

    Update Q - 8.py

--- Q - 8.py
+++ Q - 8.py
@@ -6,4 +6,9 @@ if a1 + a2 + a3 == 180.00 :
     print("Valid Triangle!")
 else:
     print("Not a Valid Triangle!")   
-    
+
+#OUTPUT:
+# Enter the first angle: 45
+# Enter the second angle: 45
+# Enter the third angle: 90
+# Valid Triangle!   
