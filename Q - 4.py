1a147af7f844e80d4b29142882b095dd2e271d9d
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:37:49 +0530

    Update Q - 4.py

--- Q - 4.py
+++ Q - 4.py
@@ -5,4 +5,7 @@ if rem == 0:
     print("YES, number is divisible by 10.")
 else :
     print("NO, number is not divisible by 10.")
-    
+
+#OUTPUT:
+# Enter the number : 45
+# NO, number is not divisible by 10. 
